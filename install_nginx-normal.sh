f0fdf4efc625fa50b1638b71da8f98c77fcd3708
xtso520ok <xtso520ok@gmail.com>
2013-06-19 18:10:20 +0800

    Add nginx-normal

--- install_nginx-normal.sh
+++ install_nginx-normal.sh
@@ -4,31 +4,32 @@
 # Purpose: One-key operation install nginx normal
 # Date:    2013-06-18
 # Author:  xtso520ok@gmail.com
-# Dsc:     OS CentOS6.3x86_64
+# Dsc:     OS Linux
 #
 
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-NAME=nginx-normal
+NAME="nginx normal"
 echo ===============================================================================
 echo Install $NAME if required...
 echo ===============================================================================
 
-which git 2>&1 | grep "no git" 
-[ $? -eq 0 ] && yum install -y git.x86_64
-
+# Source prepare
 DIR=/usr/local/src
-mkdir -p $DIR/$NAME
-cd $DIR/$NAME
-git init
-git remote add origin git@github.com:xtso520ok/nginx-normal.git
-git pull origin master
-git submodule init
-git submodule update
+cd $DIR
+S_NGINX=$DIR/nginx-1.5.1
+U_NGINX=http://nginx.org/download/nginx-1.5.1.tar.gz
+[ -e $S_NGINX.tar.z ] ||　wget $U_NGINX -O $S_NGINX.tar.gz && tar xzf $S_NGINX.tar.gz
+S_PCRE=$DIR/pcre-8.33
+U_PCRE=http://ncu.dl.sourceforge.net/project/pcre/pcre/8.33/pcre-8.33.tar.gz
+[ -e $S_PCRE.tar.z ] ||　wget $U_PCRE -O $S_PCRE.tar.gz && tar xzf $S_PCRE.tar.gz
 
+# Operate
 useradd -M -s /bin/sh --uid=515 www
-cd $DIR/$NAME/nginx
-yum install -y zlib-devel.x86_64
+
+
+# Install
+cd $
 ./configure --prefix=/usr/local/nginx-test --user=www --group=www --with-http_gzip_static_module --with-http_stub_status_module --with-pcre=$DIR/$NAME/pcre 
 make
 make install
