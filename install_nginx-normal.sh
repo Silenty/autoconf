15798d4f114a80bff9be6f2262ea667de9665dc9
xtso520ok <xtso520ok@gmail.com>
2013-06-20 00:21:28 +0800

    Modify install_nginx-normal.sh

--- install_nginx-normal.sh
+++ install_nginx-normal.sh
@@ -19,17 +19,19 @@ DIR=/usr/local/src
 cd $DIR
 S_NGINX=$DIR/nginx-1.5.1
 U_NGINX=http://nginx.org/download/nginx-1.5.1.tar.gz
-[ -e $S_NGINX.tar.z ] ||　wget $U_NGINX -O $S_NGINX.tar.gz && tar xzf $S_NGINX.tar.gz
+[ -e $S_NGINX.tar.gz ] || wget $U_NGINX -O $S_NGINX.tar.gz && tar xzf $S_NGINX.tar.gz
 S_PCRE=$DIR/pcre-8.33
 U_PCRE=http://ncu.dl.sourceforge.net/project/pcre/pcre/8.33/pcre-8.33.tar.gz
-[ -e $S_PCRE.tar.z ] ||　wget $U_PCRE -O $S_PCRE.tar.gz && tar xzf $S_PCRE.tar.gz
+[ -e $S_PCRE.tar.gz ] || wget $U_PCRE -O $S_PCRE.tar.gz && tar xzf $S_PCRE.tar.gz
 
 # Operate
-useradd -M -s /bin/sh --uid=515 www
+id www 2>&1 1>/dev/null
+[ $? -eq 0 ] || useradd -M -s /bin/sh --uid=515 www
 
 
+exit 0
 # Install
-cd $
+cd $DIR
 ./configure --prefix=/usr/local/nginx-test --user=www --group=www --with-http_gzip_static_module --with-http_stub_status_module --with-pcre=$DIR/$NAME/pcre 
 make
 make install
