f09497e07a15a08ddff81945c8411eca8c738a68
xtso520ok <xtso520ok@gmail.com>
2013-06-20 10:49:38 +0800

    Modify instal_nginx-normal

--- install_nginx-normal.sh
+++ install_nginx-normal.sh
@@ -2,9 +2,10 @@
 
 #
 # Purpose: One-key operation install nginx normal
-# Date:    2013-06-18
-# Author:  xtso520ok@gmail.com
-# Dsc:     OS Linux
+# Date:        2013-06-18
+# Author:      xtso520ok@gmail.com
+# Environment: OS Linux, gcc, gcc-c++, make
+# Des:         ...
 #
 
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
@@ -13,20 +14,41 @@ NAME="nginx normal"
 echo ===============================================================================
 echo Install $NAME if required...
 echo ===============================================================================
+NOTE_ID=$(date +%Y%m%d-%s)
 
 # Source prepare
 DIR=/usr/local/src
 cd $DIR
 S_NGINX=$DIR/nginx-1.5.1
-U_NGINX=http://nginx.org/download/nginx-1.5.1.tar.gz
-[ -e $S_NGINX.tar.gz ] || wget $U_NGINX -O $S_NGINX.tar.gz && tar xzf $S_NGINX.tar.gz
-S_PCRE=$DIR/pcre-8.33
-U_PCRE=http://ncu.dl.sourceforge.net/project/pcre/pcre/8.33/pcre-8.33.tar.gz
-[ -e $S_PCRE.tar.gz ] || wget $U_PCRE -O $S_PCRE.tar.gz && tar xzf $S_PCRE.tar.gz
+#U_NGINX=http://nginx.org/download/nginx-1.5.1.tar.gz
+#[ -e $S_NGINX.tar.gz ] || wget $U_NGINX -O $S_NGINX.tar.gz && tar xzf $S_NGINX.tar.gz
+#S_PCRE=$DIR/pcre-8.33
+#U_PCRE=http://ncu.dl.sourceforge.net/project/pcre/pcre/8.33/pcre-8.33.tar.gz
+#[ -e $S_PCRE.tar.gz ] || wget $U_PCRE -O $S_PCRE.tar.gz && tar xzf $S_PCRE.tar.gz
+#
+## Operate
+#id www 2>&1 1>/dev/null
+#[ $? -eq 0 ] || useradd -M -s /bin/sh --uid=515 www
+wget -q https://raw.github.com/xtso520ok/autoconf/master/nginx-normal/html/200.jpg -o $S_NGINX/html/200.jpg
+wget -q https://raw.github.com/xtso520ok/autoconf/master/nginx-normal/html/404.html -o $S_NGINX/html/404.html
+wget -q https://raw.github.com/xtso520ok/autoconf/master/nginx-normal/html/404.jpg -o $S_NGINX/html/404.jpg
+wget -q https://raw.github.com/xtso520ok/autoconf/master/nginx-normal/html/500.html -o $S_NGINX/html/500.html
+wget -q https://raw.github.com/xtso520ok/autoconf/master/nginx-normal/html/500.jpg -o $S_NGINX/html/500.jpg
+wget -q https://raw.github.com/xtso520ok/autoconf/master/nginx-normal/html/favicon.ico -o $S_NGINX/html/favicon.ico
+wget -q https://raw.github.com/xtso520ok/autoconf/master/nginx-normal/html/index.html -o $S_NGINX/html/index.html
+mkdir -p $S_NGINX/conf/vhost
+mv $S_NGINX/conf/nginx.conf{,.$NOTE_ID}
+wget -q https://raw.github.com/xtso520ok/autoconf/master/nginx-normal/nginx.conf -o $S_NGINX/conf/nginx.conf
+wget -q https://raw.github.com/xtso520ok/autoconf/master/nginx-normal/nginx-init -o $S_NGINX/conf/nginx-init
+wget -q https://raw.github.com/xtso520ok/autoconf/master/nginx-normal/nginx-init -o $S_NGINX/conf/nginx-sysconfig
+wget -q https://raw.github.com/xtso520ok/autoconf/master/nginx-normal/vhost/dir.i-david.org.conf -o $S_NGINX/conf/vhost/dir.i-david.org.conf
 
-# Operate
-id www 2>&1 1>/dev/null
-[ $? -eq 0 ] || useradd -M -s /bin/sh --uid=515 www
+
+cd $DIR
+sed -i 's/"1.5.1"/"1.5.1_1.0"/' $S_NGINX/src/core/nginx.h
+sed -i 's/"nginx\/"/"DS\/"/' $S_NGINX/src/core/nginx.h
+sed -i 's/"NGINX"/"DS"/' $S_NGINX/src/core/nginx.h
+sed -i 's/"Server: nginx"/"Server: DS"/' $S_NGINX/src/http/ngx_http_header_filter_module.c
 
 
 exit 0
