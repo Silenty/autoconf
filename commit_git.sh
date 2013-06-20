2a05649ae5a36a5d9291dc64ee24e536fee4d8f4
xtso520ok <xtso520ok@gmail.com>
2013-06-20 19:56:21 +0800

    Add commit_git.sh

---
+++ commit_git.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#
+# Purpose:     Auto commit to git
+# Date:        2013-06-20
+# Author:      xtso520ok@gmail.com
+# Environment: OS Linux, gcc, gcc-c++, make, zlib
+# Des:         ...
+#
+
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+git status -s
+git add *
+git status -s
+read -p "Commit description": comment
+git commit -m "$comment"
+git status -s
+git push origin master
+git status -s
+
+
